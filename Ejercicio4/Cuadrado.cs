1073c4554b483a2d380558bf182128b7f70e63c5
Juan Emiliano Cicirello <juanemilianocicirello@gmail.com>
2021-04-08 18:08:50 -0300

    Ejercicio

---
+++ Ejercicio4/Cuadrado.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio4
+{
+    class Cuadrado
+    {
+        //Atributos
+        private double longitudLado1;
+
+        //Constructores
+        public Cuadrado()
+        {
+
+        }
+
+        public Cuadrado(double valor)
+        {
+            this.longitudLado1 = valor;
+        }
+
+        //Geter Seter
+        public void getLongitud(double valor)
+        {
+            this.longitudLado1 = valor;
+        }
+
+        public double setLongitud()
+        {
+            return this.longitudLado1;
+        }
+
+        public double Perimetro()
+        {
+            return (this.longitudLado1 * 4);
+        }
+
+        public double Superficie()
+        {
+            return this.longitudLado1 * this.longitudLado1;
+        }
+
+        public override string ToString()
+        {
+            return $"El perimetro del cuadrado es: {Perimetro()}.\nLa superficie del cuadrado es: {Superficie()}";
+        }
+    }
+}
---
+++ Ejercicio4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Ejercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cuadrado c = new Cuadrado(2);
+
+            Console.WriteLine(c.ToString());
+        }
+    }
+}
