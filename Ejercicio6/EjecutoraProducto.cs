e1101f79eba5372dbd737ab5e44b7bcaa915f268
Juan Emiliano Cicirello <juanemilianocicirello@gmail.com>
2021-04-10 18:05:34 -0300

    Ejercicio

---
+++ Ejercicio6/EjecutoraProducto.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace Ejercicio6
+{
+    class EjecutoraProducto
+    {
+        static void Main(string[] args)
+        {
+            Producto p = new Producto();
+
+            string mensaje = "1-Crear producto\n2-Comprar producto\n3-Vender producto\n4-Consultar saldo\n5-Cambiar punto de pedido\n6-Cambiar maximo permitido\n7-Cambiar precio unitario\n0-Salir";
+            int opc = 0;
+            Console.WriteLine("Bienvenido al gestor de stock.\nIngrese 1 para ingresar, 0 para salir del programa");
+            opc = int.Parse(Console.ReadLine());
+
+
+            while (opc != 0)
+            {                
+                do
+                {
+                    Console.WriteLine(mensaje);
+                    opc = int.Parse(Console.ReadLine());
+                    switch (opc)
+                    {
+                        case 1:
+                            Console.WriteLine("1-Cargar todos los datos del producto\n2-Cargar Codigo y descripcion");
+                            opc = int.Parse(Console.ReadLine());
+                            switch (opc)
+                            {
+                                case 1:
+                                    int val = 0;
+                                    Console.WriteLine("Ingrese Codigo de Producto");
+                                    p.setCodigo(Console.ReadLine());
+
+                                    Console.WriteLine("Ingrese descripcion del producto");
+                                    p.setDescripcion(Console.ReadLine());
+
+                                    Console.WriteLine("Ingrese precio unitario del producto");
+                                    p.setPrecio(double.Parse(Console.ReadLine()));
+
+                                    Console.WriteLine("Ingrese punto de pedido");
+                                    p.setPuntoPedido(int.Parse(Console.ReadLine()));
+
+                                    do
+                                    {
+                                        Console.WriteLine("Ingrese maximo de stock permitido");
+                                        val = int.Parse(Console.ReadLine());
+                                        p.setMaximoStock(val);
+
+                                    } while (val < 0);
+
+                                    do
+                                    {
+                                        Console.WriteLine("Ingrese stock");
+                                        val = int.Parse(Console.ReadLine());
+                                        p.setStock(val);
+                                    } while (val > p.getMaximoStock());
+
+                                    break;
+
+                                case 2:
+                                    Console.WriteLine("Ingrese Codigo de Producto");
+                                    p.setCodigo(Console.ReadLine());
+
+                                    Console.WriteLine("Ingrese descripcion del producto");
+                                    p.setDescripcion(Console.ReadLine());
+
+                                    break;
+
+                                default:
+                                    Console.WriteLine("Opcion invalida");
+                                    opc = 0;
+                                    break;
+                            }
+                            break;
+
+                        case 2:
+                            Console.WriteLine("Ingrese la cantidad de productos que desea");
+                            p.Comprar(int.Parse(Console.ReadLine()));
+                            break;
+
+                        case 3:
+                            Console.WriteLine("Ingrese la cantidad de productos a vender");
+                            p.Vender(int.Parse(Console.ReadLine()));
+                            break;
+
+                        case 4:
+                            p.ConsultaStock();
+                            break;
+
+                        case 5:
+                            Console.WriteLine("Ingrese punto de pedido de stock");
+                            p.setPuntoPedido(int.Parse(Console.ReadLine()));
+                            break;
+
+                        case 6:
+                            Console.WriteLine("Ingrese maximo de stock permitido");
+                            p.setMaximoStock(int.Parse(Console.ReadLine()));
+                            break;
+
+                        case 7:
+                            Console.WriteLine("Ingrese nuevo precio");
+                            p.setPrecio(double.Parse(Console.ReadLine()));
+                            break;
+
+                        default:
+                            Console.WriteLine("Hasta luego!! :)");
+                            opc =0;
+                            break;
+                    }                   
+                } while (opc != 0);
+            }
+        }
+    }
+}
---
+++ Ejercicio6/Producto.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio6
+{
+    class Producto
+    {
+
+        //Atributos
+        private string descripcion;
+        private string codigo;
+        private int puntoPedido;
+        private int maxStockPer;
+        private int stockProd;
+        private double precioUni;
+
+        //Costructores
+        public Producto()
+        {
+            this.descripcion = "No se ingresó stock";
+            this.codigo = " ";
+            this.puntoPedido = 0;
+            this.maxStockPer = 0;
+            this.stockProd = 0;
+            this.precioUni = 0;
+        }
+
+        public Producto(string descripcion, string codigo, int pedido, int maxStock, int stock, double precio)
+        {
+            this.descripcion = descripcion;
+            this.codigo = codigo;
+            this.puntoPedido = pedido;
+            this.maxStockPer = maxStock;
+            this.stockProd = stock;
+            this.precioUni = precio;
+        }
+
+        //Geters Seters
+        public void setDescripcion(string descripcion)
+        {
+            this.descripcion = descripcion;
+        }
+
+        public void setCodigo(string codigo)
+        {
+            this.codigo = codigo;
+        }
+
+        public void setPuntoPedido(int pedido)
+        {
+            this.puntoPedido = pedido;
+        }
+
+        public void setMaximoStock(int stockMax)
+        {
+            this.maxStockPer = stockMax;
+        }
+
+        public void setStock(int stock)
+        {
+            this.stockProd = stock;
+        }
+
+        public void setPrecio(double precio)
+        {
+            this.precioUni = precio;
+        }
+
+        public string getDescripcion()
+        {
+            return this.descripcion;
+        }
+
+        public string getCodigo()
+        {
+            return this.codigo;
+        }
+
+        public int getPuntoPedido()
+        {
+            return this.puntoPedido;
+        }
+
+        public int getMaximoStock()
+        {
+            return this.maxStockPer;
+        }
+
+        public int getStock()
+        {
+            return this.stockProd;
+        }
+
+        public double getPrecio()
+        {
+            return this.precioUni;
+        }
+
+        //Metodo que controla las ventas e informa stock
+        public void Vender(int cantProd)
+        {
+            //este bloque de codigo permite realizar las ventas si hay stock disponible
+            if ((getStock() >= 1) && (getStock() <= getMaximoStock()))
+            {
+                this.stockProd -= cantProd;
+
+                Console.WriteLine("Cantidad\t\tProducto\t\tPrecio Unitario\t\tTotal");
+                Console.WriteLine($"{cantProd}\t\t{getDescripcion()}\t\t{getPrecio()}\t\t{getPrecio() * cantProd}");
+            }
+            else
+            {
+                Console.WriteLine("No se puede realizar la venta");
+            }
+
+            //si el stock se esta acabando informa que se debe realizar una compra
+            if (getStock() == 0)
+            {
+                Console.WriteLine($"producto sin stock");
+            }
+            else if (getStock() < getPuntoPedido())
+            {
+                Console.WriteLine($"El stock se esta acabando, quedan {getStock()} unidades. Realize una compra del producto");
+            }
+        }
+
+        //controla la compra de stock
+        public void Comprar(int cantProd)
+        {
+            //si la compra supera el maximo permitido se informara en caso contrario se realizara la compra
+            if ((cantProd + getStock()) > getMaximoStock())
+            {
+                Console.WriteLine($"La compra supera el maximo de {getMaximoStock()} unidades permitidas");
+            }
+            else
+            {
+                this.stockProd += cantProd;
+                Console.WriteLine($"La compra se ah realizado con exito, Usted dispone de {getStock()}");
+            }
+        }
+
+        public void ConsultaStock()
+        {
+            if (getStock() != 0)
+            {
+                Console.WriteLine($"Producto: {getCodigo()}\t {getDescripcion()}\nStock: {getStock()}\nPrecio unitario: {getPrecio()}\n Precio total: {getPrecio() * getStock()}");
+            }
+            else
+            {
+                Console.WriteLine($"El producto {getCodigo()}\t {getDescripcion()} esta sin stock");
+            }
+        }
+    }
+}
