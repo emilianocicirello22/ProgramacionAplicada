ede91537ca531a5840ab6f384671a070ce39cfc2
Emiliano <juanemilianocicirello@gmail.com>
2021-06-16 01:34:20 -0300

    Ejercicio

---
+++ Ejercicio4d/EjecutoraComercio.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+namespace Ejercicio4d
+{
+    class EjecutoraComercio
+    {
+        
+        static void Main(string[] args)
+        {
+            Dictionary<int,Vendedor> ven = new Dictionary<int, Vendedor>(); 
+
+            int opc = 0;
+            double ventaAnual = 0;
+
+            Console.WriteLine("Bienvenidos al programa");  
+            
+
+            do
+            {
+                Console.WriteLine("1-Ingresar Vendedor\n2-Eliminar Vendedor\n3-Ventas Anuales\n4-Venta Semestral\n0-Salir");
+                opc = int.Parse(Console.ReadLine());
+
+                switch (opc)
+                {
+
+                    case 1:
+                        Console.WriteLine("Ingrese DNI del Vendedor");
+                        int dni = int.Parse(Console.ReadLine());
+                        if (!ven.ContainsKey(dni))
+                        {
+                            ven.Add(dni ,new Vendedor());
+                            ventaAnual = ventaAnual + ven[dni].VentaPrimerSem + ven[dni].VentaSegundoSem;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Vendedor ya ingresado");
+                        }
+                        
+                        break;
+
+
+                    case 2:
+
+
+                        Console.WriteLine($"Ingrese Dni del vendedor que desea eliminar");
+                        ven.Remove(int.Parse(Console.ReadLine()));
+
+                        break;
+
+                    case 3:
+                        Console.WriteLine($"La venta anual fue de {ventaAnual}");
+                        
+                        break;
+
+                    case 4:
+                        Console.WriteLine($"Ingrese Dni del vendedor");
+                        ven[int.Parse(Console.ReadLine())].VentaMejorSemestre();
+                        break;
+                    default:
+                        if (opc == 0)
+                        {
+                            Console.WriteLine("Gracias por utilizar la APP");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Opcion Incorrecta");
+                        }
+                        break;
+
+                }
+            } while (opc != 0);
+        }
+    }    
+}
---
+++ Ejercicio4d/Vendedor.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio4d
+{
+    class Vendedor
+    {
+
+        private string nombre;
+        private int dni;
+        private float ventaPrimerSem;
+        private float ventaSegundoSem;
+        public int Dni { get => dni; set => dni = value; }
+        public float VentaPrimerSem { get => ventaPrimerSem; set => ventaPrimerSem = value; }
+        public float VentaSegundoSem { get => ventaSegundoSem; set => ventaSegundoSem = value; }
+
+        public Vendedor()
+        {
+            Console.WriteLine("Ingrese nombre Y apellido del vendedor");
+            nombre = Console.ReadLine();
+
+            Console.WriteLine("Ingrese DNI del vendedor");
+            dni = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese venta del primer semestre");
+            ventaPrimerSem = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese venta del segundo semestre");
+            ventaSegundoSem = float.Parse(Console.ReadLine());
+        }
+
+        public void VentaMejorSemestre()
+        {
+            string mensaje = "";
+                        
+            if (VentaPrimerSem > VentaSegundoSem)
+            {
+                mensaje = $"La mejor venta se produjo en el Primer semestre y fue por: {VentaPrimerSem}";
+            }
+            else if (VentaPrimerSem < VentaSegundoSem)
+            {
+                mensaje = $"La mejor venta se produjo en el Segundo semestre y fue por: {VentaSegundoSem}";
+            }
+            else
+            {
+                mensaje = $"Las ventas fueron iguales en ambos semestres: {VentaPrimerSem}";
+            }
+
+            Console.WriteLine(mensaje);
+
+        }
+
+        public override string ToString()
+        {
+            return $"Nombre: {this.nombre}\nDNI: {this.dni}";
+        }
+    }
+}
